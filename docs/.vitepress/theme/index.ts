b8d92f7c6aea961c2c7ba5167536e0af0c164307
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2026-01-13 12:04:42 -0300

    chore: add vercel analytics to docs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,6 @@
 import DefaultTheme from 'vitepress/theme';
+import { Analytics } from '@vercel/analytics/vue';
+import { h } from 'vue';
 import DemoActions from './components/DemoActions.vue';
 import DemoEventLog from './components/DemoEventLog.vue';
 import DemoFrame from './components/DemoFrame.vue';
@@ -6,6 +8,11 @@ import './custom.css';
 
 export default {
   ...DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'layout-bottom': () => h(Analytics),
+    });
+  },
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp?.(ctx);
     ctx.app.component('DemoActions', DemoActions);
